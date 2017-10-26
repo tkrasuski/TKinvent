860762bbff30a8dc715b4725638f0655a0a87694
Tomek <tomek@MacBook-Pro-jak-by-co.local>
2017-10-26 13:57:52 +0200

    xlsx files handling

---
+++ xl.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# this is xlsx handling module
+import settings as stt
+import json
+class XlHandler(object):
+    def __init__(self):
+        self.file = None
+        self.js_data = None
+        
