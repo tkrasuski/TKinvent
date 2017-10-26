bc5ab8ba9362193196ca54148188b0eed71377e3
Tomek <tomek@MacBook-Pro-jak-by-co.local>
2017-10-26 18:38:47 +0200

    xlsx files handling, file format in docs

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 
     {"content": [
         {
-            "line":{"part_no":"nazwa pozycji 1", "qty":1}
+            "line":{"part_no":"nazwa pozycji 1", "description":"part description", "qty":1}
         },
         {
             "line":{"part_no":"nazwa pozycji 2", "qty":12}
--- xl.py
+++ xl.py
@@ -2,8 +2,29 @@
 # this is xlsx handling module
 import settings as stt
 import json
+from openpyxl import load_workbook
+
 class XlHandler(object):
     def __init__(self):
         self.file = None
         self.js_data = None
-        
+        self.wb = None
+        self.ws = None
+        self.data =[]
+    def loadFile(self, file_):
+        'Loads workbook file and sets it as class '
+        bfile = open(file_, 'rb')
+        self.wb = load_workbook(filename=bfile,read_only=True)
+    def parseFile(self):
+        self.ws = self.wb.active
+        for row in self.ws.iter_rows(min_row=2, max_col=5, max_row=8):
+            drow = {}
+            for cell in row:
+                print cell.column, ':', cell.value
+                if cell.column == 1:
+                    drow['part_no']=cell.value
+                if cell.column == 2:
+                    drow['description']=cell.value
+                if cell.column == 4:
+                    drow['qty']=cell.value
+            self.data.append(dict(line=drow))
